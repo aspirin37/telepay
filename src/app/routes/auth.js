b15268cbbafdb5a9ea87c8a2009f7d7f4e93e827
Artem Smirnov <artem@postradar.ru>
2018-03-15 16:04:35 +0300

    rm

--- src/app/routes/auth.js
+++ src/app/routes/auth.js
@@ -1,9 +1,6 @@
 import Login from '@modules/auth/login';
 import Registration from '@modules/auth/registration';
 import Recovery from '@modules/auth/recovery';
-import NewPass from '@modules/auth/new_pass';
-import Activate from '@modules/auth/activate';
-import AdminEntr from '@modules/auth/admin-entr';
 import AuthLayout from '@modules/auth/layout';
 import Logout from '@modules/auth/logout';
 
@@ -29,21 +26,6 @@ export default {
     component: Recovery,
     meta: { auth: false }
     }, {
-    path: 'activate',
-    name: 'activate',
-    component: Activate,
-    meta: { auth: false }
-    }, {
-    path: 'new-pass',
-    name: 'new_pass',
-    component: NewPass,
-    meta: { auth: false }
-    }, {
-    path: 'admin-ent',
-    name: 'admin-ent',
-    component: AdminEntr,
-    meta: { auth: false }
-  },{
     path: 'logout',
     name: 'logout',
     component: Logout,
