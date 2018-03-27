9922f4f31207a5263c73399b7d4fcbadfdc4e516
Artem Smirnov <artem@postradar.ru>
2018-03-27 12:44:39 +0300

    poka tak

--- src/app/utils/check.js
+++ src/app/utils/check.js
@@ -6,7 +6,7 @@ import LS from '@utils/local_storage';
 export function routerAuthHandle(app, to, from, next) {
   app.$store.commit('TOGGLE_LOADING', true);
   let auth_key = LS.get('auth_key');
-  if(!app.$store.user && !auth_key) {
+  if(!app.$store.state.user && !auth_key) {
     if(to.meta.auth) {
       app.$notifystr.danger('access denied');
       if(app.$route.name !== 'login') {
@@ -17,21 +17,27 @@ export function routerAuthHandle(app, to, from, next) {
       next();
     }
     app.$store.commit('TOGGLE_LOADING', false);
-  } else if (auth_key) {
+  } else if (auth_key && !app.$store.state.user) {
     getUser(app, auth_key, next);
+  } else {
+    next();
+    app.$store.commit('TOGGLE_LOADING', false);
   }
 }
 
 export function mountAuthHandle(app, to) {
   app.$store.commit('TOGGLE_LOADING', true);
   let auth_key = LS.get('auth_key');
-  if(!app.$store.user && !auth_key) {
+  if(!app.$store.state.user && !auth_key) {
     if(to.meta.auth) {
       app.$router.replace({ name: 'login' });
     }
     app.$store.commit('TOGGLE_LOADING', false);
-  } else if (auth_key) {
+  } else if (auth_key && !app.$store.state.user) {
     getUser(app, auth_key);
+  } else {
+    next();
+    app.$store.commit('TOGGLE_LOADING', false);
   }
 }
 
