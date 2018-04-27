75a819f51acc4d4e8cf11d72466b7ab50c0be02f
Konstantin <onefivefournine@gmail.com>
2018-04-27 15:56:32 +0300

    early timeframes + grad fix

--- src/app/components/timeframes/index.vue
+++ src/app/components/timeframes/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div @mousedown="mousePressed=true" @mouseup="mousePressed=false" class="timeframes">
-    {{mousePressed}}
-    <div class="cell-row" :key="i" v-for="(dayOfWeek,i) in ['пн','вт','ср','чт','пт','сб','вс']">
-      <div class="cell" :key="hour" v-for="hour in Array.from(Array(24).keys())">
-        {{dayOfWeek+' '+hour}}
+    <div class="cell-row" :key="i" v-for="(dayOfWeek,i) in weekDays">
+      <div class="cell d-flex" :class="{'border-0':!cell.y || !cell.x,'bg-success':cell.selected}" :key="cell.y+cell.x" v-for="cell in cells[i]" @mouseover="handleMouseOver(cell)" @mousedown="handleMouseDown(cell)" @mouseup="initalCell = null">
+        <span class="align-self-center" v-if="!cell.x">{{dayOfWeek}}</span>
+        <span class="align-self-end" v-if="cell.x && !cell.y">{{(cell.x-1) | getTime}}</span>
       </div>
     </div>
   </div>
@@ -11,7 +11,12 @@
 <script>
 export default Vue.extend({
   data() {
-    return { mousePressed: false };
+    return {
+      mousePressed: false,
+      initalCell: null,
+      weekDays: ['', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'],
+      cells: Array.from(Array(8).fill(Array.from(Array(25).keys()))).map((row, y) => row.map(i => ({ selected: false, x: i, y })))
+    };
   },
   props: {
     timeframes: {
@@ -20,11 +25,57 @@ export default Vue.extend({
     }
   },
   created() {},
-  methods: {}
+  methods: {
+    handleMouseDown(cell) {
+      if (cell && cell.x && cell.y) {
+        cell.selected = !cell.selected;
+        this.initalCell = cell;
+      }
+    },
+    handleMouseOver(cell) {
+      if (this.mousePressed && cell && this.initalCell) {
+        this.cells.forEach(row => {
+          row.forEach(c => {
+            c.selected =
+              c.x &&
+              c.y &&
+              // x+y+ == down/right
+              ((c.x <= cell.x && c.x >= this.initalCell.x && c.y <= cell.y && c.y >= this.initalCell.y) ||
+                // x-y- == up/left
+                (c.x >= cell.x && c.x <= this.initalCell.x && c.y >= cell.y && c.y <= this.initalCell.y) ||
+                // x+y- == up/right
+                (c.x <= cell.x && c.x >= this.initalCell.x && c.y >= cell.y && c.y <= this.initalCell.y) ||
+                // x-y+ == down/left
+                (c.x >= cell.x && c.x <= this.initalCell.x && c.y <= cell.y && c.y >= this.initalCell.y));
+          });
+        });
+      }
+    }
+  },
+  filters: {
+    getTime(val) {
+      let hours = +val;
+      if (hours < 10) {
+        hours = '0' + hours;
+      }
+      return `${hours}:00`;
+    }
+  }
 });
 </script>
-<style>
-/* .timeframes {
-
-} */
+<style lang="scss">
+.timeframes {
+  .cell-row {
+    display: flex;
+    justify-content: space-around;
+  }
+  .cell {
+    border: 1px solid #ccc;
+    padding: 5px;
+    white-space: nowrap;
+    width: 100%;
+    height: 50px;
+    user-select: none;
+  }
+}
 </style>
