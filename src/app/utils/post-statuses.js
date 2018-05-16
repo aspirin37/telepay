719a26e46abeeb476522669866e98175bfdb7cb6
Evgeniy <evgeniy@stereoflo.ru>
2018-05-16 12:50:29 +0300

    nano fix

--- src/app/utils/post-statuses.js
+++ src/app/utils/post-statuses.js
@@ -3,6 +3,6 @@ export default {
     '1': 'Отклонен', // STATUS_DECLINED
     '2': 'Ожидает публикации', // STATUS_PENDING
     '3': 'Опубликован', // STATUS_POSTED
-    '4': 'Публикуется', // STATUS_IN_PROGRESS
+    '4': 'Ожидает публикации', // STATUS_IN_PROGRESS
     '5': 'Новый', // STATUS_NEW
 };
