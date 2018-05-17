ab1b97669dd14239b73a7583c390aa250ebdad0a
Константин Васильев <onefivefournine@iMac-Konstantin.local>
2018-05-17 16:14:22 +0300

    фикс циферок в каталоге

--- src/app/components/navigation/index.vue
+++ src/app/components/navigation/index.vue
@@ -10,63 +10,75 @@ import { mapGetters } from 'vuex';
 import { NotificationApi } from '@services/api';
 
 export default Vue.extend({
-  components: { onOff, dropDown, dropDownMenuItem },
-  data() {
-    return {
-      isVisible: false,
-      Logo,
-      showDD: false,
-      notifications: [],
-      notificationsCount: 0,
-      updateTimeout: null,
-      year: moment().year()
-    };
-  },
-  created() {
-    this.getNotificationList();
-  },
-  destroyed() {
-    clearTimeout(this.updateTimeout);
-  },
+    components: { onOff, dropDown, dropDownMenuItem },
+    data() {
+        return {
+            isVisible: false,
+            Logo,
+            showDD: false,
+            notifications: [],
+            notificationsCount: 0,
+            updateTimeout: null,
+            year: moment().year()
+        };
+    },
+    created() {
+        this.getNotificationList();
+    },
+    destroyed() {
+        clearTimeout(this.updateTimeout);
+    },
 
-  computed: {
-    isAdvert: {
-      get() {
-        return this.$store.state.is_advert;
-      },
-      set(val) {
-        LS.set('is_advert', val);
-        this.$store.commit('CHANGE_STATE', { key: 'is_advert', value: val });
-        this.$router.push({ name: val ? 'catalog' : 'channels:list' });
-      }
+    computed: {
+        isAdvert: {
+            get() {
+                return this.$store.state.is_advert;
+            },
+            set(val) {
+                LS.set('is_advert', val);
+                this.$store.commit('CHANGE_STATE', { key: 'is_advert', value: val });
+                this.$router.push({ name: val ? 'catalog' : 'channels:list' });
+            }
+        },
+        ...mapGetters({
+            hasUser: 'hasUser',
+            getUsername: 'getUsername',
+            balance: 'getUserBalance'
+        }),
+        logoVisible() {
+            return this.$route.name !== 'main' && !this.$route.fullPath.includes('auth');
+        },
+        isAuthorized() {
+            return this.hasUser && LS.get('auth_key');
+        }
     },
-    ...mapGetters({
-      hasUser: 'hasUser',
-      getUsername: 'getUsername',
-      balance: 'getUserBalance'
-    }),
-    logoVisible() {
-      return this.$route.name !== 'main' && !this.$route.fullPath.includes('auth');
-    },
-    isAuthorized() {
-      return this.hasUser && LS.get('auth_key');
-    }
-  },
-  methods: {
-    async getNotificationList() {
-      clearTimeout(this.updateTimeout);
-      let { items, total } = await NotificationApi.list();
-      this.notifications = items;
-      this.notificationsCount = total;
-      this.updateTimeout = setTimeout(this.getNotificationList, 1e4);
-    },
-    async setIsRead(notificationId) {
-      let cachedNotifications = clone(this.notifications);
-      this.notifications = this.notifications.filter(n => n.notificationId !== notificationId);
+    methods: {
+        toggleDD() {
+            this.showDD = !this.showDD
+            if (this.showDD) this.$refs.ddmenu.scrollTop = 0;
+        },
+        blurHandler() {
+            this.closeDDTimeout = setTimeout(() => {
+                this.showDD = false
+            }, 200);
+        },
+        stopBlur() {
+            clearTimeout(this.closeDDTimeout);
+        },
+        async getNotificationList() {
+            clearTimeout(this.updateTimeout);
+            let { items, total } = await NotificationApi.list();
+            this.notifications = items;
+            this.notificationsCount = total;
+            this.updateTimeout = setTimeout(this.getNotificationList, 1e4);
+        },
+        async setIsRead(notificationId) {
+            let cachedNotifications = clone(this.notifications);
+            this.notifications = this.notifications.filter(n => n.notificationId !== notificationId);
 
-      await NotificationApi.markAsRead({ notificationId });
-      this.getNotificationList();
+            await NotificationApi.markAsRead({ notificationId });
+            this.getNotificationList();
+        }
     }
-  }
 });
 </script>
--- src/app/filters/cut-sum.js
+++ src/app/filters/cut-sum.js
@@ -1,9 +1,12 @@
 export default function(val, num = 0) {
-    if (val < 1e3) {
-        return val;
-    } else if (val >= 1e3 && val < 1e6) {
-        return (val / 1e3).toFixed(num) + 'k';
-    } else if (val >= 1e6) {
-        return (val / 1e6).toFixed(num) + 'M';
+    if(typeof val !== 'number' && typeof val !== 'string') return val;
+    if(typeof val === 'string') val = parseFloat(val);
+
+    if(val < 1e3) {
+        return +val.toFixed(num);
+    } else if(val >= 1e3 && val < 1e6) {
+        return(+(val / 1e3).toFixed(num)) + 'k';
+    } else if(val >= 1e6) {
+        return(+(val / 1e6).toFixed(num)) + 'M';
     }
 }
