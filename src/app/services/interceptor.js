2c2836dc03367aec0ccb650864760fa5c872b338
Maksim Molovcev <molir10@mail.ru>
2018-06-02 17:21:22 +0300

    403 redirect

--- src/app/services/interceptor.js
+++ src/app/services/interceptor.js
@@ -3,31 +3,31 @@ import { App } from '../main';
 import loaderBlacklist from '@utils/loader-blacklist';
 import notifystrBlacklist from '@utils/notifystr-blacklist';
 // response handler
-Vue.http.interceptors.push((request, next) => {
-    if(App && !loaderBlacklist.find(el => new RegExp(el).test(request.url))) {
-        App.$store.commit('TOGGLE_LOADING', true);
+Vue.http.interceptors.push( ( request, next ) => {
+    if ( App && !loaderBlacklist.find( el => new RegExp( el ).test( request.url ) ) ) {
+        App.$store.commit( 'TOGGLE_LOADING', true );
     }
-    next(response => {
-        if(App && !loaderBlacklist.find(el => new RegExp(el).test(request.url))) App.$store.commit('TOGGLE_LOADING', false);
-        if(response.ok) {
-            if(request.method !== 'GET' && !notifystrBlacklist.find(el => new RegExp(el).test(response.url))) {
-                App.$notifystr.success('Успешно!', response.data.message || '');
+    next( response => {
+        if ( App && !loaderBlacklist.find( el => new RegExp( el ).test( request.url ) ) ) App.$store.commit( 'TOGGLE_LOADING', false );
+        if ( response.ok ) {
+            if ( request.method !== 'GET' && !notifystrBlacklist.find( el => new RegExp( el ).test( response.url ) ) ) {
+                App.$notifystr.success( 'Успешно!', response.data.message || '' );
             }
         } else {
-            if(response && response.data && typeof response.data === 'object') {
-                if(response.status === 401) {
-                    App.$router.push({ name: 'logout' });
+            if ( response && response.data && typeof response.data === 'object' ) {
+                if ( response.status === 401 || response.status === 403 ) {
+                    App.$router.push( { name: 'logout' } );
                 } else {
-                    for(let err in response.data) {
-                        App.$notifystr.danger('Ошибка!', response.data[err]);
+                    for ( let err in response.data ) {
+                        App.$notifystr.danger( 'Ошибка!', response.data[ err ] );
                     }
                 }
-            } else if(response && response.data && typeof response.data === 'string' && response.status !== 500) {
-                App.$notifystr.danger('Ошибка!', response.data);
+            } else if ( response && response.data && typeof response.data === 'string' && response.status !== 500 ) {
+                App.$notifystr.danger( 'Ошибка!', response.data );
             }
-            if(response.status === 500) {
-                App.$notifystr.danger('Критическая ошибка сервера!', 'Обратитесь в службу поддержки!');
+            if ( response.status === 500 ) {
+                App.$notifystr.danger( 'Критическая ошибка сервера!', 'Обратитесь в службу поддержки!' );
             }
         }
-    });
-});
+    } );
+} );
