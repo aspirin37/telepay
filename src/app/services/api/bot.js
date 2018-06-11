3a2dc51f841e3ff406bfe3f991185fb2e287748e
Konstantin <onefivefournine@gmail.com>
2018-06-11 17:00:18 +0300

    fixes

--- src/app/services/api/bot.js
+++ src/app/services/api/bot.js
@@ -1,7 +1,21 @@
-import { _r, _e, genRes } from './_utils';
+import { _r, _e } from './_utils';
 
-const botRes = genRes('bot', ['GET']);
+let OFFICE_URL;
+let protocol = (document.location && document.location.protocol) || 'http:';
+
+switch(process.env.url) {
+    case 'loc':
+        OFFICE_URL = 'http://0.0.0.0:8080';
+        break;
+    case 'dev':
+        OFFICE_URL = `${protocol}//dev.telepay.io`;
+        break;
+    default:
+        OFFICE_URL = `${protocol}//telepay.io`;
+        break;
+}
 
 export default {
-    getChannelInfo: (username) => botRes('channel-info').get(username).then(_r).catch(_e),
+    getChannelInfo: (username) => Vue.resource(OFFICE_URL + '/bot/channel-info').get(username).then(_r).catch(_e),
+
 };
