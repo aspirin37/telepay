a2ee82876b86657c278b0558d9080a47d7289c88
Konstantin <onefivefournine@gmail.com>
2018-07-09 13:03:02 +0300

    fix?

--- webpack.config.js
+++ webpack.config.js
@@ -23,64 +23,66 @@ const PATHS = {
 };
 
 const common = function (env) {
+    console.log(env);
+    console.log(process.version);
     return merge([{
-            entry: {
-                'bundle': path.resolve(__dirname, PATHS.source + '/app/main.js'),
-            },
-            mode: env.NODE_ENV,
-            output: {
-                path: PATHS.build,
-                publicPath: '/',
-                filename: '[name].[hash].js',
-            },
-            resolve: {
-                extensions: ['.js', '.vue'],
-                modules: ['node_modules'],
-                alias: {
-                    '@assets': path.resolve(PATHS.source, 'assets'),
-                    '@modules': path.resolve(PATHS.source, 'app/modules'),
-                    '@components': path.resolve(PATHS.source, 'app/components'),
-                    '@services': path.resolve(PATHS.source, 'app/services'),
-                    '@store': path.resolve(PATHS.source, 'app/store'),
-                    '@routes': path.resolve(PATHS.source, 'app/routes'),
-                    '@utils': path.resolve(PATHS.source, 'app/utils'),
-                    '@mixins': path.resolve(PATHS.source, 'app/mixins'),
-                    '@filters': path.resolve(PATHS.source, 'app/filters'),
-                    'vue$': 'vue/dist/vue.esm.js',
-                },
-            },
-            plugins: [
-                // new BundleAnalyzerPlugin({ analyzerPort: 5541 }),
-                // new webpack.optimize.ModuleConcatenationPlugin(),
-                new HtmlWebpackPlugin({
-                    inject: 'body',
-                    template: path.resolve(__dirname, PATHS.source + '/app/index.html'),
-                    minify: {
-                        caseSensitive: true,
-                        collapseWhitespace: true,
-                    },
-                }),
-                new webpack.optimize.SplitChunksPlugin(),
-                new webpack.ContextReplacementPlugin(/moment[\\/]locale$/, /^\.\/(en|ru)$/), // load only en/ru locale
-                new webpack.DefinePlugin({
-                    'process.env': {
-                        url: '\'' + env.url + '\'',
-                        NODE_ENV: '\'' + env.NODE_ENV + '\'',
-                    },
-                }),
-                new webpack.ProvidePlugin({
-                    Vue: ['vue/dist/vue.esm.js', 'default'], // WARNING!!! DO NOT DELETE, Vue needs to be imported in almost every component/module
-                    swal: path.resolve(__dirname, 'src/app/components/swal'),
-                    moment: 'moment',
-                }),
-                new CleanWebpackPlugin(['build/bundle*']),
-            ],
+        entry: {
+            'bundle': path.resolve(__dirname, PATHS.source + '/app/main.js'),
         },
-        images(),
-        fonts(),
-        html(),
-        babel(),
-        media(),
+        mode: env.NODE_ENV,
+        output: {
+            path: PATHS.build,
+            publicPath: '/',
+            filename: '[name].[hash].js',
+        },
+        resolve: {
+            extensions: ['.js', '.vue'],
+            modules: ['node_modules'],
+            alias: {
+                '@assets': path.resolve(PATHS.source, 'assets'),
+                '@modules': path.resolve(PATHS.source, 'app/modules'),
+                '@components': path.resolve(PATHS.source, 'app/components'),
+                '@services': path.resolve(PATHS.source, 'app/services'),
+                '@store': path.resolve(PATHS.source, 'app/store'),
+                '@routes': path.resolve(PATHS.source, 'app/routes'),
+                '@utils': path.resolve(PATHS.source, 'app/utils'),
+                '@mixins': path.resolve(PATHS.source, 'app/mixins'),
+                '@filters': path.resolve(PATHS.source, 'app/filters'),
+                'vue$': 'vue/dist/vue.esm.js',
+            },
+        },
+        plugins: [
+            // new BundleAnalyzerPlugin({ analyzerPort: 5541 }),
+            // new webpack.optimize.ModuleConcatenationPlugin(),
+            new HtmlWebpackPlugin({
+                inject: 'body',
+                template: path.resolve(__dirname, PATHS.source + '/app/index.html'),
+                minify: {
+                    caseSensitive: true,
+                    collapseWhitespace: true,
+                },
+            }),
+            new webpack.optimize.SplitChunksPlugin(),
+            new webpack.ContextReplacementPlugin(/moment[\\/]locale$/, /^\.\/(en|ru)$/), // load only en/ru locale
+            new webpack.DefinePlugin({
+                'process.env': {
+                    url: '\'' + env.url + '\'',
+                    NODE_ENV: '\'' + env.NODE_ENV + '\'',
+                },
+            }),
+            new webpack.ProvidePlugin({
+                Vue: ['vue/dist/vue.esm.js', 'default'], // WARNING!!! DO NOT DELETE, Vue needs to be imported in almost every component/module
+                swal: path.resolve(__dirname, 'src/app/components/swal'),
+                moment: 'moment',
+            }),
+            new CleanWebpackPlugin(['build/bundle*']),
+        ],
+    },
+    images(),
+    fonts(),
+    html(),
+    babel(),
+    media(),
     ]);
 };
 
