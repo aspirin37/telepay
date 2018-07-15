f62f52e77563c030bbbde566e80e363ac2882bce
aspirin37 <molir10@mail.ru>
2018-07-15 18:59:36 +0300

    ?

--- src/app/services/check-user.js
+++ src/app/services/check-user.js
@@ -1,32 +1,32 @@
 import WebStorage from '@utils/storage';
 import { UserApi } from '@services/api';
 
-async function checkUserAvailable(App) {
-    let auth_key = WebStorage.get('auth_key');
-    let isAuth = window.location.pathname.includes('auth/');
-    if(!auth_key && !isAuth) {
+async function checkUserAvailable( App ) {
+    let auth_key = WebStorage.get( 'auth_key' );
+    let isAuth = window.location.pathname.includes( 'auth/' );
+    if ( !auth_key && !isAuth ) {
         // нет токена
-        App.$router.replace({ name: 'login' });
-    } else if(auth_key) {
-        Vue.http.headers.common['X-API-TOKEN'] = auth_key;
+        App.$router.replace( { name: 'login' } );
+    } else if ( auth_key ) {
+        Vue.http.headers.common[ 'X-API-TOKEN' ] = auth_key;
         let user = await UserApi.getUser()
-            .catch((err) => {
-                if(err.status === 401) {
-                    App.$router.push({ name: 'logout' });
-                    App.$notifystr.danger('Ошибка доступа!', 'Токен авторизации устарел!');
+            .catch( ( err ) => {
+                if ( err.status === 401 ) {
+                    App.$router.push( { name: 'logout' } );
+                    App.$notifystr.danger( 'Ошибка доступа!', 'Токен авторизации устарел!' );
                 }
-            });
+            } );
 
-        if(user && user.userId) {
-            let tg_id = WebStorage.get('tg_id');
-            if(tg_id) {
-                await UserApi.update({ telegram_id: tg_id });
-                WebStorage.rm('tg_id');
-                App.$router.replace({ name: App.$route.name, query: {} });
+        if ( user && user.userId ) {
+            let tg_id = WebStorage.get( 'tg_id' );
+            if ( tg_id ) {
+                await UserApi.update( { telegram_id: tg_id } );
+                WebStorage.rm( 'tg_id' );
+                App.$router.replace( { name: App.$route.name, query: {} } );
             }
-            App.$store.commit('SET_USER', user);
-            if(isAuth) {
-                App.$router.replace({ name: 'catalog' });
+            App.$store.commit( 'SET_USER', user );
+            if ( isAuth ) {
+                App.$router.replace( { name: 'catalog' } );
             }
         }
     }
